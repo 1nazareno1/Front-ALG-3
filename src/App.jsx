b95b91e4c5df5a3da34aa77e059a596124e8f2ba
Benjamin Zapata <benjaminzapata1998@gmail.com>
2025-04-13 17:43:17 -0300

    MUI, router-dom instalados

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,43 @@
+import { Box, Card, CardContent, Typography } from "@mui/material";
+
 function App() {
   return (
     <>
-      <h1 className="id">Algoritmos y estructura de datos III</h1>
-      <button>Apretame</button>
+      <Box>
+        <Card sx={() => ({ margin: "16px" })}>
+          <CardContent>
+            <Typography variant="h4">Titulo</Typography>
+            <Typography variant="body1">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore
+              sunt expedita provident, officiis magnam officia repellat dolorum
+              voluptas in ab ipsa vel ullam cumque tenetur, culpa, corrupti
+              natus dignissimos repudiandae.
+            </Typography>
+          </CardContent>
+        </Card>
+        <Card sx={() => ({ margin: "16px" })}>
+          <CardContent>
+            <Typography variant="h4">Titulo</Typography>
+            <Typography variant="body1">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore
+              sunt expedita provident, officiis magnam officia repellat dolorum
+              voluptas in ab ipsa vel ullam cumque tenetur, culpa, corrupti
+              natus dignissimos repudiandae.
+            </Typography>
+          </CardContent>
+        </Card>
+        <Card sx={() => ({ margin: "16px" })}>
+          <CardContent>
+            <Typography variant="h4">Titulo</Typography>
+            <Typography variant="body1">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore
+              sunt expedita provident, officiis magnam officia repellat dolorum
+              voluptas in ab ipsa vel ullam cumque tenetur, culpa, corrupti
+              natus dignissimos repudiandae.
+            </Typography>
+          </CardContent>
+        </Card>
+      </Box>
     </>
   );
 }
