861b5c7d5d4776f02676432a6d92dab0fc7cb735
Benjamin Zapata <benjaminzapata1998@gmail.com>
2025-05-17 13:41:58 -0300

    HomePage style

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,22 +1,25 @@
-import { Box } from '@mui/material'
-import { TestComponent } from '../../components/commons/TestComponent'
+import { Box, useMediaQuery, useTheme } from "@mui/material";
+import { TestComponent } from "../../components/commons/TestComponent";
 
 export const HomePage = () => {
+  const theme = useTheme();
+  const isDesktop = useMediaQuery(theme.breakpoints.up("lg"));
+
   return (
     <Box>
       <Box
         sx={(theme) => ({
-          alignItems: 'center',
+          alignItems: "center",
           background: theme.palette.secondary.dark,
-          display: 'flex',
+          display: "flex",
           gap: theme.spacing(5),
           height: theme.spacing(20),
-          justifyContent: 'center',
-          width: 'calc(100vw - 78px)',
+          justifyContent: "center",
+          width: isDesktop ? "calc(100vw - 78px)" : "100vw",
         })}
       >
         <TestComponent />
       </Box>
     </Box>
-  )
-}
+  );
+};
