2614f4f81c032dc3b5634ae7f4d0d1fe931fca46
Benjamin Zapata <benjaminzapata1998@gmail.com>
2025-08-20 19:58:10 -0300

    Redux, sonner, ReduxToolkit

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,9 +1,12 @@
 import { Box, useMediaQuery, useTheme } from '@mui/material'
-import { UserCardComponent } from '../../components/posts/UserCardComponent'
+import { useDispatch } from 'react-redux'
+import { useEffect } from 'react'
+import { getUserById } from '../../redux/slices/usersSlice'
 
 export const HomePage = () => {
   const theme = useTheme()
   const isDesktop = useMediaQuery(theme.breakpoints.up('lg'))
+  const dispatch = useDispatch()
 
   return (
     <Box>
