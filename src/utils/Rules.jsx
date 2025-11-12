42221b03d6c9dc991517b82153293482aeaeb4e5
Juan Martin Arrizubieta <juanarrizubieta@hotmail.com>
2025-11-12 19:23:47 -0300

    cambio de ubicacion

--- src/utils/Rules.jsx
+++
@@ -1,9 +0,0 @@
-export const RulesText = `Aun cuando tratamos de editar o eliminar cualquier mensaje que contenga algo inapropiado, abusivo, obsceno, vulgar, de odio, amenazante, sexo, o algún otro contenido considerado invasivo a la privacidad de las personas o que viole leyes vigentes, es imposible revisar todos los mensajes. Por lo tanto acepta que todos los mensajes publicados en estos foros expresan las opiniones de sus autores originales y no la de los administradores y moderadores (excepto en mensajes publicados por ellos mismos) por lo cual no se les considerará responsables.
-
-Usted está de acuerdo en no publicar material abusivo, obsceno, vulgar, de odio, amenazante, orientado sexualmente, o ningún otro que de alguna forma viole leyes vigentes.
-
-Si publicase material de esa índole su cuenta de acceso al foro será cancelada y su proveedor de acceso a internet será informado. La dirección IP de todos los mensajes es guardada para ayudar a cumplir estas normas. Usted está de acuerdo en que el webmaster, administrador y moderadores de este foro tienen el derecho de borrar, editar, mover o cerrar cualquier tema en cualquier momento si lo consideran conveniente. Además acepta que cualquier información que introduzcas en este foro sea almacenada en una base de datos y que se guarden "cookies" en tu ordenador para guardar la información de la información del inicio de sesión.
-
-Como usuario acepta que toda la información que ingrese sea almacenada en una base de datos. Aún cuando esta información no será proporcionada a terceros sin su consentimiento, el equipo del foro no puede responsabilizarse por intentos de hackers que puedan llevar a que esta información se vea comprometida.
-
-Al registrarse usted aceptará todas estas condiciones y cualquier otra regla que el administrador disponga.`
