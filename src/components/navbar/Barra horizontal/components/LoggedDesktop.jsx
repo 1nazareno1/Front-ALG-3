1189661f6167c2ac5d735908ea417c3aa4dd3337
Benjamin Zapata <benjaminzapata1998@gmail.com>
2025-12-05 23:29:28 -0300

    Se agrega pagina para crear post, ahora se captura la sesion activa y la recupera

--- src/components/navbar/Barra horizontal/components/LoggedDesktop.jsx
+++ src/components/navbar/Barra horizontal/components/LoggedDesktop.jsx
@@ -1,27 +1,35 @@
-import { Box, Tooltip, Typography } from '@mui/material'
-import { resetAuthState } from '../../../../redux/slices/authSlice'
-import { Logout } from '@mui/icons-material'
-import { NotificationMock } from '../../../../utils/Commons'
-import { NotificationsMenu } from './NotificationsMenu'
-import { useDispatch } from 'react-redux'
-import { useState } from 'react'
+import { Box, Tooltip, Typography } from "@mui/material";
+import { resetAuthState } from "../../../../redux/slices/authSlice";
+import { Logout } from "@mui/icons-material";
+import { NotificationMock } from "../../../../utils/Commons";
+import { NotificationsMenu } from "./NotificationsMenu";
+import { useDispatch } from "react-redux";
+import { useState } from "react";
 
 export const LoggedDesktop = ({ username }) => {
-  const dispatch = useDispatch()
-  const [notifications, setNotifications] = useState(NotificationMock)
+  const dispatch = useDispatch();
+  const [notifications, setNotifications] = useState(NotificationMock);
 
   const closeNotification = (id) => {
     const newNotifications = notifications.filter(
       (notification) => notification.id !== id
-    )
-    setNotifications(newNotifications)
-  }
+    );
+    setNotifications(newNotifications);
+  };
 
   return (
-    <Box display={'flex'} gap={3} alignItems={'center'}>
-      <Typography color="primary">
-        Hola{' '}
-        <Typography component={'span'} fontWeight={500}>
+    <Box display={"flex"} gap={3} alignItems={"center"}>
+      <Typography
+        color="primary"
+        sx={{
+          maxWidth: "30ch",
+          overflow: "hidden",
+          textOverflow: "ellipsis",
+          whiteSpace: "nowrap",
+        }}
+      >
+        Hola{" "}
+        <Typography component={"span"} fontWeight={500}>
           {username}
         </Typography>
       </Typography>
@@ -33,13 +41,13 @@ export const LoggedDesktop = ({ username }) => {
         <Box
           height={24}
           onClick={() => {
-            dispatch(resetAuthState())
-            location.reload()
+            dispatch(resetAuthState());
+            location.reload();
           }}
         >
-          <Logout color="primary" sx={{ cursor: 'pointer' }} />
+          <Logout color="primary" sx={{ cursor: "pointer" }} />
         </Box>
       </Tooltip>
     </Box>
-  )
-}
+  );
+};
